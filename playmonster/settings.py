b8103b48affcfded5523f0660efae3ad431894ae
root <root@b6b4cb6241ef>
2017-05-05 00:05:37 +0000

    add deploy

--- playmonster/settings.py
+++ playmonster/settings.py
@@ -32,7 +32,6 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 # Application definition
 
 INSTALLED_APPS = [
-    'bootsrap_admin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
