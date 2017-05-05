6522b1c49f43d7376311bfc9d1ce2db7fa64b29c
root <root@b6b4cb6241ef>
2017-05-05 00:13:57 +0000

    add whitenosies

--- playmonster/settings.py
+++ playmonster/settings.py
@@ -48,6 +48,7 @@ BOOTSTRAP_ADMIN_SIDEBAR_MENU = True
 MIDDLEWARE_CLASSES = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
